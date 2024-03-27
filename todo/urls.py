67366acbecca399c5725d5ad39d7f6f99171c7de
Kien Bui <kaybi9x@gmail.com>
2024-03-27 15:16:20 +0700

    finish

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import sign_up, sign_in, todo
+from .views import sign_up, sign_in, todo, user_logout, edit_todo, del_todo, check_todo
 
 urlpatterns = [
     path('', todo, name="todoList"),
     path('sign-up/', sign_up, name='signUp'),
     path('login/', sign_in, name="login"),
+    path("logout/", user_logout, name="logout"),
+    path("edit/<int:id>", edit_todo, name="edit"),
+    path("del/<int:id>", del_todo, name="del"),
+    path("check/<int:id>", check_todo, name="check")
 ]
--- todo/views.py
+++ todo/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Todo
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import AnonymousUser
 
 # Create your views here.
 def sign_up(request):
@@ -11,7 +12,7 @@ def sign_up(request):
     pwd = request.POST.get('pwd')
     new_user = User.objects.create_user(fnm, email, pwd)
     new_user.save()
-    return redirect('/todo/login')
+    return redirect('/login')
   return render(request, 'signup.html')
 
 
@@ -20,23 +21,55 @@ def sign_in(request):
     username = request.POST.get('username')
     pwd = request.POST.get('pwd')
     user = authenticate(request, username = username, password = pwd)
-    print(user)
     if user is not None:
       login(request, user)
-      return redirect('/todo/')
+      return redirect('/')
     else:
-      return redirect('/todo/login/') 
+      return redirect('/login/') 
   return render(request, 'login.html')
 
+def user_logout(request):
+  logout(request)
+  return redirect('/login/')
+
 def todo(request):
-  if request.user is None:
-    return redirect('/todo/login')
+  if isinstance(request.user, AnonymousUser):
+    return redirect('/login/')
+  
   if request.method == 'POST':
     title = request.POST.get('title')
     new_todo = Todo.objects.create(title=title, user=request.user)
     new_todo.save()
     res = Todo.objects.filter(user=request.user).order_by('-date')
-    return redirect('/todo/', {'res': res})
+    return redirect('/', {'res': res})
+  
   res = Todo.objects.filter(user=request.user).order_by('-date')   
   
   return render(request, 'todo.html', {'User_Info': request.user, 'res': res})
+
+def edit_todo(request, id):
+  data = Todo.objects.get(no=id)
+  if request.method == 'POST':
+    title = request.POST.get('title')
+    data.title = title
+    data.save()
+    res = Todo.objects.filter(user=request.user).order_by('-date')
+    return redirect('/', {'res': res})
+    
+  return render(request, 'edit.html', {'data': data})
+
+def del_todo(request, id):
+  data = Todo.objects.get(no=id)
+  data.delete()
+  res = Todo.objects.filter(user=request.user).order_by('-date')
+  return redirect('/', {'res': res})
+
+def check_todo(request, id):
+  data = Todo.objects.get(no=id)
+  data.is_done = not data.is_done
+  data.save()
+  res = Todo.objects.filter(user=request.user).order_by('-date')
+  return redirect('/', {'res': res})
+    
+    
+  
